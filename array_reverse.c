a0bdcda7cf420e64cf0bcf7f3b21468a96432f85
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2026-04-17 09:15:08 +0600

    try to get element from the array by using reverse way

--- array_reverse.c
+++ array_reverse.c
@@ -13,6 +13,7 @@ int main ()
     1.need to start from the (n-1) becasuse array index always start from the 0. So the last element is at index (n-1), not n .
 
     2. Most Important is condition here. Condition (i>=0) means we continune the loop until we reach the first elemnt (index 0).
+    
    */
 
     {
