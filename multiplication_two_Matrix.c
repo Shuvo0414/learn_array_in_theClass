d2af943056a35962e5851201e6d2ae6645507c0b
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2026-04-22 12:12:58 +0600

    add comment for better understand

--- multiplication_two_Matrix.c
+++ multiplication_two_Matrix.c
@@ -1,68 +1,80 @@
 #include <stdio.h>
 
-int main()
+int main() 
 {
-    int r1, c1, r2, c2;
+    // Declare variables for matrix dimensions
+    int rows1, cols1, rows2, cols2;
 
+    // Take input for Matrix A size
     printf("Enter rows and cols of Matrix A:\n");
-    scanf("%d %d", &r1, &c1);
+    scanf("%d %d", &rows1, &cols1);
 
+    // Take input for Matrix B size
     printf("Enter rows and cols of Matrix B:\n");
-    scanf("%d %d", &r2, &c2);
+    scanf("%d %d", &rows2, &cols2);
 
-    // Check condition
-    if (c1 != r2)
+    // Check condition for matrix multiplication
+    // Rule: cols of A must be equal to rows of B
+    if (cols1 != rows2)
     {
         printf("Matrix multiplication not possible!\n");
-        return 0;
+        return 0; // Stop the program if condition fails
     }
     
+    // Declare matrices
+    int A[rows1][cols1], B[rows2][cols2], C[rows1][cols2];
 
-    int A[r1][c1], B[r2][c2], C[r1][c2];
-
+    // Input elements of Matrix A
     printf("Enter elements of A:\n");
-    for (int i = 0; i < r1; i++)
-        for (int j = 0; j < c1; j++)
+    for (int i = 0; i < rows1; i++)
+        for (int j = 0; j < cols1; j++)
             scanf("%d", &A[i][j]);
 
+    // Input elements of Matrix B
     printf("Enter elements of B:\n");
-    for (int i = 0; i < r2; i++)
-        for (int j = 0; j < c2; j++)
+    for (int i = 0; i < rows2; i++)
+        for (int j = 0; j < cols2; j++)
             scanf("%d", &B[i][j]);
 
-    printf("The elements of A matrixs\n");
-    for(int i = 0; i < r1; i++) {
-        for(int j = 0; j < c1; j++) {
+    // Print Matrix A
+    printf("The elements of A matrix:\n");
+    for(int i = 0; i < rows1; i++) {
+        for(int j = 0; j < cols1; j++) {
             printf("\t%d\t", A[i][j]);
         }
         printf("\n");
     }
-    printf("The elements of B matrixs\n");
-    for(int i = 0; i < r2; i++) {
-        for(int j = 0; j < c2; j++) {
+
+    // Print Matrix B
+    printf("The elements of B matrix:\n");
+    for(int i = 0; i < rows2; i++) {
+        for(int j = 0; j < cols2; j++) {
             printf("\t%d\t", B[i][j]);
         }
         printf("\n");
     }
 
-    // Multiplication
-    for (int i = 0; i < r1; i++)
+    // Matrix Multiplication Logic
+    // C[i][j] = sum of (A[i][k] * B[k][j])
+    for (int i = 0; i < rows1; i++)
     {
-        for (int j = 0; j < c2; j++)
+        for (int j = 0; j < cols2; j++)
         {
-            C[i][j] = 0; 
+            C[i][j] = 0; // Initialize result cell
 
-            for (int k = 0; k < c1; k++)
+            // Multiply row of A with column of B
+            for (int k = 0; k < cols1; k++)
             {
                 C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
 
+    // Print Result Matrix
     printf("Result matrix:\n");
-    for (int i = 0; i < r1; i++)
+    for (int i = 0; i < rows1; i++)
     {
-        for (int j = 0; j < c2; j++)
+        for (int j = 0; j < cols2; j++)
         {
             printf("\t%d\t", C[i][j]);
         }
